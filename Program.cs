a25863de60cae92368918a174c11fb1218477dba
mohammad <mohammad@>
2026-03-30 19:05:36 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RA22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your text");
+            string text = Console.ReadLine();
+            Console.WriteLine(" # Choose 1 to 9 :\n 1.Exit\n 2.Change text\n 3.Count vowels\n 4.Count upper letters\n 5.Count lower letters\n 6.Transform to upper\n 7.Transform to lower\n 8.Enter a letter to find the first occurience\n 9..... all of the ...\n 10.Radar level");
+            int q = Convert.ToInt32(Console.ReadLine());
+
+            if (q == 1)
+                Console.WriteLine("#--#--# END #--#--#");
+
+            else if (q==2)
+            {
+                Console.WriteLine(" #Enter ur new text");
+                string NewText = Console.ReadLine(); 
+            }
+
+            else if (q==3)
+            {
+                int Counter_Vowels = StringHelper.CountVowels(text);
+                Console.WriteLine("--RESULT : "+Counter_Vowels);
+            }
+
+            else if (q==4)
+            {
+                int CounterUpp = StringHelper.CountUpper(text);
+                Console.WriteLine("--RESULT : "+CounterUpp);
+            }
+
+            else if (q == 5)
+            {
+                int Countlow = StringHelper.CountLower(text);
+                Console.WriteLine("-- RESULT : " + Countlow);
+            }
+
+            else if (q == 6)
+            {
+                string upp;
+                upp = StringHelper.Upper(text);
+                Console.WriteLine("-- RESULT : " + upp);
+            }
+
+            else if (q == 7)
+            {
+                string low;
+                low= StringHelper.Lower(text);
+                Console.WriteLine("-- RESULT : "+low);
+            }
+
+            else if (q == 8)
+            {
+                Console.WriteLine(" #Gimme ur letter sir...");
+                char letter = Convert.ToChar(Console.ReadLine());
+                int FirstOcc = StringHelper.First(text, letter);
+                Console.WriteLine("-- RESULT : " + FirstOcc);
+            }
+
+            else if (q == 9)
+            {
+                char letter = Convert.ToChar(Console.ReadLine());
+                int x = StringHelper.AllFound(text, letter);
+                Console.WriteLine("-- RESULT : "+x);
+            }
+            
+
+        }
+    }
+}
---
+++ StringHelper.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace RA22
+{
+    internal class StringHelper
+    {
+        public static int CountLower(string str)
+        {
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] >='a' && str[i] <='z')
+                    count ++;
+            }
+            return count;
+        }
+
+        public static int CountUpper(string str)
+        {
+            int count = 0;
+            for(int i = 0; i < str.Length;i++)
+            {
+                if (str[i] >= 'A' && str[i] <= 'Z')
+                    count++;
+            }
+            return count;
+        }
+
+        public static string Upper(string str)
+        {
+            int num = 0;
+            string newstr = ("");
+            for (int i = 0;i < str.Length;i++)
+            {
+                if (str[i] >= 'a' && str[i] <= 'z')
+                {
+                    num = str[i];
+                    num = num - 32;
+                    newstr += (char)num;
+                }
+                else
+                    newstr += str[i];
+            }
+            return newstr;
+        }
+
+
+        public static int First(string str,Char target)
+        {
+            for (int i = 0; i<str.Length ; i++)
+            {
+                if (str[i] == target)
+                    return i+1;
+            }
+            return -1;
+        }
+
+        public static string Lower(string str)
+        {
+            int num = 0;
+            string newstr = ("");
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] >= 'A' && str[i] <= 'Z')
+                {
+                    num = str[i];
+                    num += 32;
+                    newstr += (char)num;
+                }
+                else
+                    newstr += str[i];
+            }
+            return newstr;
+        }
+
+        public static int AllFound(string str, char target)
+        {
+            int place;
+            Console.WriteLine("Positions :");
+            bool fo = false;
+            for (int i = 0; i < str.Length; i++)
+                if (str[i] == target)
+                {
+                    place = i + 1;
+                    Console.Write(place);
+                    Console.Write("...");
+                    fo = true;
+                    return place;
+                }
+                int r = 0;
+                return r;
+
+        }
+
+        public static int CountVowels(string txt)
+        {
+            int NumOfVowels = 0;
+            foreach (char Vo in txt) //بتصير ب "فور" بس هيك احسن وانضف
+            {
+                if (Vo == 'a' || Vo == 'e' || Vo == 'i' || Vo == 'u' || Vo == 'o' || Vo == 'A' ||
+                    Vo == 'O' || Vo == 'U' || Vo == 'I' || Vo =='E')
+                    NumOfVowels++;
+            }
+                return NumOfVowels;
+        }
+    }   
+}
